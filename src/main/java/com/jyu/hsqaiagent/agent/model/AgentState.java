4ca0e477ea8be7c04dc245cd5387ff24b09337ba
minH_IT <1878427982@qq.com>
2025-11-01 15:45:32 +0800

    feat:项目day7 智能体开发

---
+++ src/main/java/com/jyu/hsqaiagent/agent/model/AgentState.java
@@ -0,0 +1,11 @@
+package com.jyu.hsqaiagent.agent.model;
+
+public enum AgentState {
+    IDLE,
+
+    RUNNING,
+
+    FINISHED,
+
+    ERROR
+}
--- src/main/java/com/jyu/hsqaiagent/app/LoveApp.java
+++ src/main/java/com/jyu/hsqaiagent/app/LoveApp.java
@@ -16,6 +16,7 @@ import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -151,6 +152,22 @@ public class LoveApp {
         String content = chatResponse.getResult().getOutput().getText();
         log.info("content: {}", content);
         return content;
+    }
+
+    /**
+     * AI 基础对话（支持多轮对话记忆，SSE 流式传输）
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public Flux<String> doChatByStream(String message, String chatId) {
+        return chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
+                .stream()
+                .content();
     }
 
     public String doChatWithMcp(String message, String chatId) {
